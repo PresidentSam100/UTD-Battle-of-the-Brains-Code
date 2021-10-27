cfc9e7e8db62c3c4f7ee7b97a0f14d3e3f62097e
PresidentSam100 <56271444+PresidentSam100@users.noreply.github.com>
2021-10-27 14:07:03 -0500

    Update C.java

--- C.java
+++ C.java
@@ -1,28 +1,35 @@
 import java.math.BigInteger;
 import java.util.Scanner;
-
+// Problem Link: https://open.kattis.com/contests/vm8cd3/problems/grazedgrains
 public class C {
     public static void main(String[] args) {
+        // initialize scanner
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        // arrays for describing the circles
         int[] X = new int[n];
         int[] Y = new int[n];
         int[] R = new int[n];
+        // Read input
         for (int i = 0; i < n; i++)
         {
             int x = sc.nextInt();
             int y = sc.nextInt();
             int r = sc.nextInt();
-            X[i] = x;
-            Y[i] = y;
-            R[i] = r;
+            X[i] = x; // x-coordinate of center
+            Y[i] = y; // y-coordinate of center
+            R[i] = r; // radius of circle
         }
+        // estimate area using Monte Carlo        
         double num=0;
+        // iterate through x
         for (double xx = -10; xx <= 20; xx+=0.01)
         {
+            // iterate through y
             for (double yy = -10; yy <= 20; yy+=0.01)
             {
-                boolean work = false;
+                // check if point is contained within at least one circle
+                boolean work = false;            
                 for (int i = 0; i < n; i++)
                 {
                     if (isInside(X[i], Y[i], R[i], xx, yy)) {
@@ -35,12 +42,17 @@ public class C {
 
             }
         }
+        // divide to satisfy the expected area
         System.out.println(num/10000);
     }
 
-
+    // Circle forumula: (x - h)^2 + (y - k)^2 = r^2
+    // (x,y) center of circle
+    // (h,k) test point
+    // r: radius
     static boolean isInside(int circle_x, int circle_y, int rad, double x, double y)
     {
+        // test point will be in circle if it would be mapped on another circle with the same center and smaller radius
         if ((x - circle_x) * (x - circle_x) + (y - circle_y) * (y - circle_y) <= rad * rad)
             return true;
         else
