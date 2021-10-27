a208acd98c7e15c3878e2958bb549e8f1c0729f9
PresidentSam100 <56271444+PresidentSam100@users.noreply.github.com>
2021-10-27 14:21:12 -0500

    Update F.cpp

--- F.cpp
+++ F.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
-
+// Problem Link: https://open.kattis.com/contests/vm8cd3/problems/more10
 typedef std::unordered_map<std::string, int> Dictionary;
 typedef std::vector<std::pair<int, int>> Queries;
 typedef std::string String;
 typedef std::vector<std::pair<int,std::vector<std::pair<int,std::vector<int>>>>> Tree;
 #define TREE_INIT 26, std::make_pair(-1, std::vector<std::pair<int,std::vector<int>>>(26, std::make_pair(-1, std::vector<int>(26,-1))))
 
+// define a graph-like structure
 struct Struc {
     std::vector<int> p; 
     Struc(int n) : p(n, -1) { }
@@ -36,7 +37,7 @@ bool approve(Struc& uf, Queries& qq) {
     }
     return true;
 }
-
+// rhyming case 1
 void rr11(Struc& uf, Tree& tree, String& str, int i, int i1) {
     if (tree[i1].first == -1) {
         tree[i1].first = i;
@@ -53,6 +54,7 @@ void rr11(Struc& uf, Tree& tree, String& str, int i, int i1) {
         }
     }
 }
+// rhyming case 2
 void rr22(Struc& uf, Tree& tree, String& str, int i, int i1, int i2) {
     if (tree[i1].first != -1) {
         uf.conn(i, tree[i1].first);
@@ -65,6 +67,7 @@ void rr22(Struc& uf, Tree& tree, String& str, int i, int i1, int i2) {
         }
     }
 }
+// rhyming case 3
 void rr33(Struc& uf, Tree& tree, String& str, int i, int i1, int i2, int i3) {
     if (tree[i1].first != -1) {
         uf.conn(i, tree[i1].first);
@@ -76,6 +79,7 @@ void rr33(Struc& uf, Tree& tree, String& str, int i, int i1, int i2, int i3) {
         uf.conn(tree[i1].second[i2].second[i3],i);
     }
 }
+// test each rhyme case to see if it works
 void art(Struc& uf, Tree& tree, String& str, int i) {
     switch (str.size()) {
         case 1:
@@ -88,6 +92,7 @@ void art(Struc& uf, Tree& tree, String& str, int i) {
             rr33(uf, tree, str, i, str[str.size()-1]-'a', str[str.size()-2]-'a', str[str.size()-3]-'a');
     }
 }
+// index of method to search
 int iof(Dictionary& s2, String& str, int& nin) {
     int index;
     if (s2.find(str) == s2.end()) {
@@ -98,6 +103,7 @@ int iof(Dictionary& s2, String& str, int& nin) {
     }
     return index;
 }
+// bfs method (maybe)
 void read(Struc& uf, Queries& qq, int n) {
     String a, b, op;
     int aa, bb, nin = 0;
@@ -114,6 +120,7 @@ void read(Struc& uf, Queries& qq, int n) {
     }
 }
 
+// input readers
 void sc() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(NULL);
@@ -121,6 +128,7 @@ void sc() {
 }
 
 int main() {
+    // read input and print output
     sc();
     int n;
     std::cin >> n;
